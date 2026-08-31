801e5d021132e7827bb774bb58f3068208d38614
overkill489 <overkill.alt.489@gmail.com>
2026-08-31 11:34:13 -0500

    Load More button

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const ExploreItems = () => {
   const [exploreItems, setExploreItems] = useState([]);
+  const [visibleItems, setVisibleItems] = useState(8);
 
   useEffect(() => {
     fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore")
@@ -12,22 +13,18 @@ const ExploreItems = () => {
 
   function filterNft(filter) {
     if (filter === "price_low_to_high") {
-      setExploreItems(
-        exploreItems.slice().sort(
-          (a, b) => (a.price) - (b.price),
-        ),
-      );
+      setExploreItems(exploreItems.slice().sort((a, b) => a.price - b.price));
     }
     if (filter === "price_high_to_low") {
-      setExploreItems(exploreItems.slice().sort(
-        (a,b) => (b.price) -(a.price)
-      ))
+      setExploreItems(exploreItems.slice().sort((a, b) => b.price - a.price));
     }
     if (filter === "likes_high_to_low") {
-      setExploreItems(exploreItems.slice().sort(
-        (a,b) => b.likes - a.likes
-      ))
+      setExploreItems(exploreItems.slice().sort((a, b) => b.likes - a.likes));
     }
+  }
+
+  function loadMore() {
+    setVisibleItems(visibleItems + 4);
   }
 
   return (
@@ -44,7 +41,7 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {exploreItems.map((item) => (
+      {exploreItems.slice(0, visibleItems).map((item) => (
         <div
           key={item.id}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -103,9 +100,9 @@ const ExploreItems = () => {
         </div>
       ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        <button id="loadmore" className="btn-main lead" onClick={loadMore}>
+          Load More
+        </button>
       </div>
     </>
   );
